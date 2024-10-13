a85bba818dc1881e6d55c4befc688f10abe9a3da
Adam Young <atyoung1997@gmail.com>
2024-10-13 17:03:54 -0500

    removeElement

---
+++ removeElement.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//int removeElement(vector<int>& nums, int val) {
+//	int lead{ 0 };
+//	int trail{ 0 };
+//
+//	while (lead < nums.size()) {
+//		if (nums[lead] != val) {
+//			if (nums[trail] == val) { // case lead != val, trail == val
+//				nums[trail] = nums[lead];
+//				nums[lead] = val;
+//				trail++;
+//				lead++;
+//			}
+//			else { // case lead != val, trail != val
+//				trail++;
+//				lead++;
+//			}
+//		}
+//		else {
+//			if (nums[trail] == val) { // case lead == val, trail == val
+//				lead++;
+//			}
+//			else { // case lead == val, trail != val
+//				trail++;
+//			}
+//		}
+//	}
+//	return trail;
+//
+//}
+//
+//
+//
+//
+//int main() {
+//	vector<int> test_nums_1{ 1, 5, 6, 7, 7, 7, 8, 9, 7, 5, 7};
+//	int test_val_1{ 7 };
+//	removeElement(test_nums_1, test_val_1);
+//	return 0;
+//}
