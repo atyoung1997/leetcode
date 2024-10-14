4b9cf4b0e0cb058b6d7e69d9002cb7a364d952c8
Adam Young <atyoung1997@gmail.com>
2024-10-13 22:40:10 -0500

    finished removeDuplicates

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -8,36 +8,21 @@ int removeDuplicates(vector<int>& nums) {
 	int lead{ 0 };
 	int trail{ 0 };
 	unordered_map<int, int> umap{};
-	for (auto& a : nums) {
-		cout << a << ' ';
-	}
-	cout << '\n';
 
-	while (lead < nums.size()) {
-		int lead_num{ nums[lead] };
-		int trail_num{ nums[trail] };
-		
-		if (umap.find(lead_num) == umap.end()){
-			if (umap.find(trail_num) == umap.end()) {
-				lead++;
-				trail++;
+	while (lead < nums.size()) {	
+		if (umap.find(nums[lead]) == umap.end()) {
+			if (umap.find(nums[trail]) != umap.end()) {
+				nums[trail] = nums[lead];
 			}
-			else {
-				nums[trail] = lead_num;
-				lead++;
-				trail++;
-			}
-			umap.insert(pair<int, int>(lead_num, 1));
+			umap.insert(pair<int, int>(nums[lead], 1));
+			lead++;
+			trail++;
 		}
 		else {
 			lead++;
 		}
 	}
-	cout << trail << '\n';
-	for (auto& a : nums) {
-		cout << a << ' ';
-	}
-	return 0;
+	return trail;
 }
 
 int main() {
