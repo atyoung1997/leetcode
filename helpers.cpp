774803bcc77a33112ffe92c2981034ed03111eb4
Adam Young <atyoung1997@gmail.com>
2024-10-15 19:29:39 -0500

    start helpers file

---
+++ helpers.cpp
@@ -0,0 +1,9 @@
+#include "helpers.h"
+
+void printVec(vector<int> v) {
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i];
+	}
+	cout << '\n';
+}
+
---
+++ helpers.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void printVec(std::vector<int> v);
+
