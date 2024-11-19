ad09f66a2e0f397f47e52b44c1777d1229ad8429
Adam Young <atyoung1997@gmail.com>
2024-11-18 18:56:06 -0600

    singleNumber.cpp

---
+++ singleNumber.cpp
@@ -0,0 +1,16 @@
+//#include "helpers.h"
+//int singleNumber(vector<int>& nums) {
+//	int result{0};
+//	for (int i = 0; i < nums.size(); i++) {
+//		result ^= nums[i];
+//	}
+//	return result;
+//}
+//
+//int main() {
+//	vector<int> test1{ 7 };
+//	vector<int> test2{ 4, 2, 1, 2, 1 };
+//	cout << singleNumber(test1) << '\n';
+//	cout << singleNumber(test2) << '\n';
+//	return 0;
+//}
