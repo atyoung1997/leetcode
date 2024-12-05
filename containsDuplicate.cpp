a84f634dc76902e443338fb8410fd1cc164f50a5
Adam Young <atyoung1997@gmail.com>
2024-12-04 21:59:18 -0600

    containsDuplicate

---
+++ containsDuplicate.cpp
@@ -0,0 +1,20 @@
+//#include "helpers.h"
+//#include <unordered_map>
+//using namespace std;
+//
+//
+//bool containsDuplicate(vector<int>& nums) {
+//	unordered_map<int, bool> seen_nums{};
+//	for (int num : nums) {
+//		if(seen_nums.find(num) != seen_nums.end()){
+//			return true;
+//		}
+//		seen_nums.insert({ num, true });
+//	}
+//	return false;
+//}
+//
+//int main() {
+//
+//	return 0;
+//}
